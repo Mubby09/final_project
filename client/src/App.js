034f1a8199e1440f418f4e0c7ccec41dbb7aaa3e
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-04-27 15:23:08 +0100

    semantic-ui-react and semantic-ui-css addition for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+import Navbar from "./components/Navbar";
+
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
   return (
-    <div>
-      <h1>Final project</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
